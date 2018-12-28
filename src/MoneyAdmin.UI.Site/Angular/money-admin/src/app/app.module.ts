4ffca66d0f0eb84549b06d3bc3f4a8d5fa549080
Victor do Amor Divino <victordoamordivino@gmail.com>
2018-12-27 23:21:14 -0300

    Add Concept layout

--- src/MoneyAdmin.UI.Site/Angular/money-admin/src/app/app.module.ts
+++ src/MoneyAdmin.UI.Site/Angular/money-admin/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    CardComponent,
+    PageHeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/MoneyAdmin.UI.Site/Angular/money-admin/src/app/page-header/page-header.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-page-header',
+    templateUrl: 'page-header.component.html'
+})
+export class PageHeaderComponent {
+    @Input() title: string;
+    @Input() text: string;
+}
