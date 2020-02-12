3a71efb3c3fd1e515d3027870f0a500aa7a0861f
lucaslima10077 <lucaslima10077@gmail.com>
2020-02-12 12:03:58 -0300

    Added CreateAccountCommand in Domain

--- src/MoneyAdmin.Domain/Commands/CreateAccountCommand.cs
+++ src/MoneyAdmin.Domain/Commands/CreateAccountCommand.cs
@@ -1,6 +1,19 @@
-﻿namespace MoneyAdmin.Domain.Commands
+﻿using MoneyAdmin.Domain.Core.Commands;
+using System;
+
+namespace MoneyAdmin.Domain.Commands
 {
-    public class CreateAccountCommand
+    public class CreateAccountCommand : CommandBase
     {
+        public CreateAccountCommand()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; protected set; }
+
+        public string Name { get; set; }
+
+        public decimal InitialValue { get; set; }
     }
 }
