51951833fc8031ab02d72717a59ea5b201f20aa6
lucaslima10077 <lucaslima10077@gmail.com>
2020-02-12 11:44:06 -0300

    added commandBase and commandResult

---
+++ src/MoneyAdmin.Domain.Core/Commands/CommandResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MoneyAdmin.Domain.Core.Commands
+{
+    public class CommandResult
+    {
+
+        public CommandResult(Exception exception) : this()
+        {
+            Exception = exception;
+        }
+
+        public CommandResult()
+        {
+        }
+        public Exception Exception { get; }
+
+        public bool IsSuccess => Exception == null;
+
+        public static CommandResult Success() => new CommandResult();
+
+        public static implicit operator bool(CommandResult result) => result.IsSuccess;
+
+        public static implicit operator CommandResult(Exception exception) => new CommandResult(exception);
+    }
+}
