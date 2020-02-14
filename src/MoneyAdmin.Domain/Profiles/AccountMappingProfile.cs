c2301520871ea629610239931be8a11b69eef4b8
Lucas Lima <55801810+lucaslima10077@users.noreply.github.com>
2020-02-14 10:58:42 -0300

    add autoMapper (#13)

---
+++ src/MoneyAdmin.Domain/Profiles/AccountMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MoneyAdmin.Domain.Commands;
+
+namespace MoneyAdmin.Domain.Profiles
+{
+    public class AccountMappingProfile : Profile
+    {
+        public AccountMappingProfile()
+        {
+            CreateMap<CreateAccountCommand, Account>()
+                .ForMember(dest => dest.Amount, m => m.MapFrom(a => a.InitialValue));
+        }
+    }
+}
