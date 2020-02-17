cfc2b205495deee936b2934629ff761930661fa7
Lucas Lima <55801810+lucaslima10077@users.noreply.github.com>
2020-02-17 12:32:13 -0300

    Add fluent validation  (#14)

---
+++ src/MoneyAdmin.Domain/Validators/CreateAccountCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MoneyAdmin.Domain.Commands;
+
+namespace MoneyAdmin.Domain.Validators
+{
+    public class CreateAccountCommandValidator : AbstractValidator<CreateAccountCommand>
+    {
+        public CreateAccountCommandValidator()
+        {
+            RuleFor(x => x.Name).Length(3, 60).WithMessage("The name must be greater than 2 less than 60");
+        }
+    }
+}
