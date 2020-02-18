587c140e71e3ccc6e12255a673f434869485d39a
lucaslima10077 <lucaslima10077@gmail.com>
2020-02-18 09:02:27 -0300

    Added Tests to propertie IsValid in CreateAccountCommand

---
+++ src/Tests/MoneyAdmin.Domain.UnitTests/Commands/CreateAccountCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyAdmin.Domain.Commands
+{
+    [TestClass]
+    public class CreateAccountCommandTests
+    {
+        [TestMethod]
+        public void CreateAccountWithNameValidShouldTrue()
+        {
+            // Arrange
+            var command = new CreateAccountCommand();
+            command.Name = "NuConta";
+
+            // Action
+            var result = command.IsValid;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(61)]
+        [DataRow(2)]
+        public void CreateAccountWithNameInvalidShouldFalse(int nameLegth)
+        {
+            // Arrange
+            var command = new CreateAccountCommand();
+            command.Name = new string('a', nameLegth);
+
+            // Action
+            var result = command.IsValid;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
