4da041e3067464bd4912376b1d15120e1a9662ef
Lucas Lima <55801810+lucaslima10077@users.noreply.github.com>
2020-02-18 15:06:23 -0300

    Add validation to CreateAccount (#17)

---
+++ src/Tests/MoneyAdmin.Domain.UnitTests/Commands/CreateAccountCommandTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyAdmin.Domain.Commands
+{
+    [TestClass]
+    public sealed class CreateAccountCommandTests
+    {
+        [TestMethod]
+        public void CreateAccountWithNameValidShouldTrue()
+        {
+            // Arrange
+            var command = new CreateAccountCommand
+            {
+                Name = "NuConta"
+            };
+
+            // Assert
+            command.IsValid.Should().BeTrue();
+        }
+
+        [DataTestMethod]
+        [DataRow(61)]
+        [DataRow(2)]
+        [DataRow(0)]
+        public void CreateAccountWithNameInvalidShouldFalse(int nameLegth)
+        {
+            // Arrange
+            var command = new CreateAccountCommand
+            {
+                Name = new string('a', nameLegth)
+            };
+
+            // Assert
+            command.IsValid.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CreateAccountWithNameNullShouldFalse()
+        {
+            // Arrange
+            var command = new CreateAccountCommand
+            {
+                Name = null
+            };
+
+            // Assert
+            command.IsValid.Should().BeFalse();
+        }
+    }
+}
