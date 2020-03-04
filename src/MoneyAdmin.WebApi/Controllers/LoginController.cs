2abd7b4644f111ed0d4e3fb66d646e53527ccf45
lucaslima10077 <lucaslima10077@gmail.com>
2020-03-04 18:51:05 -0300

    added Authenticate to Login and generate access token

---
+++ src/MoneyAdmin.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MoneyAdmin.Domain.Models;
+using MoneyAdmin.Infra.Data.Repositories;
+using MoneyAdmin.WebApi.Services;
+
+namespace MoneyAdmin.WebApi.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody]Login model)
+        {
+            var user = LoginRepository.Get(model.UserName, model.PassWord);
+
+            if (user == null)
+                return BadRequest("Not Found");
+
+            var token = TokenServices.GeneratorToken(user);
+            user.PassWord = "";
+            return new
+            {
+                user,
+                token
+            };
+        }
+    }
+}
