81959cf1a351362d7b943c32063662b016ed9dc4
lucaslima10077 <lucaslima10077@gmail.com>
2020-03-04 18:48:23 -0300

    added LoginRepository to get user in memory

---
+++ src/MoneyAdmin.Infra.Data/Repositories/LoginRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MoneyAdmin.Domain.Models;
+
+namespace MoneyAdmin.Infra.Data.Repositories
+{
+    public static class LoginRepository
+    {
+        public static Login Get(string userName, string password)
+        {
+            var users = new List<Login>
+            {
+                new Login { UserName = "moneyadmin", PassWord = "1234", Role = "user" }
+            };
+            return users.Where(u => u.UserName.ToLower() == userName.ToLower() && u.PassWord == password).FirstOrDefault();
+        }
+    }
+}
