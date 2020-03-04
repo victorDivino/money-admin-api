0ddbe89e21ecbcfde9c69bf4579e5147299cc3cb
lucaslima10077 <lucaslima10077@gmail.com>
2020-03-04 18:40:40 -0300

    added entity Login

---
+++ src/MoneyAdmin.Domain/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿using MoneyAdmin.Domain.Core.Models;
+
+namespace MoneyAdmin.Domain.Models
+{
+    public class Login : Entity
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Role { get; set; }
+    }
+}
